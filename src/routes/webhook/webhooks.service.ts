d06e5a14f4da94be1e78cfdfedeb4d5833be61b1
Trevor Wolfe <trevo1992@gmail.com>
2019-05-10 14:25:30 -0700

    created the layout for most ofthe routes and endpoints

---
+++ src/routes/webhook/webhooks.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+
+@Injectable()
+export class WebhooksService {
+    async recieveHook(webhookId: string): Promise<string> {
+        return 'recieve aws webhooks here';
+    }
+}
