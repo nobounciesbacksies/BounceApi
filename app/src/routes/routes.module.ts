5b0135cb8015449d07246b053253c9505b9955c3
Trevor Wolfe <trevo1992@gmail.com>
2019-07-19 09:00:34 -0700

    added the interceptor to routes module

--- app/src/routes/routes.module.ts
+++ app/src/routes/routes.module.ts
@@ -1,4 +1,5 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { BlacklistModule } from './blacklist/blacklist.module';
 import { WebhooksModule } from './webhook/webhooks.module';
 import { CompressionMiddleware } from '@nest-middlewares/compression';
@@ -9,9 +10,16 @@ import { BlacklistController } from './blacklist/blacklist.controller';
 import { WebhooksController } from './webhook/webhooks.controller';
 import { HealthcheckModule } from './healthcheck/healthcheck.module';
 import { AuthModule } from '../auth/auth.module';
+import { TransformResponse } from '../middleware/response.interceptor';
 
 @Module({
   imports: [BlacklistModule, WebhooksModule, HealthcheckModule, AuthModule],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformResponse,
+    },
+  ],
 })
 export class RoutesModule {
   /**
